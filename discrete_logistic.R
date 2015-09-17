72a255c4aa513768636c33c8d7333d74923d09ab
shawnmceachin <mceachinshawn@gmail.com>
2015-09-16 21:12:13 -0700

    create a logistic growth model
    and plot graphs with various parameters

---
+++ discrete_logistic.R
@@ -0,0 +1,62 @@
+# a model for discrete logistic growth
+N0=10
+rr=.5
+KK=100
+ttMax=50
+NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
+NN[1] <- N0
+for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+print(NN)
+
+# convert the script into a function and plot it
+discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun(N0=10, rr=0.5, KK=100, ttMax=50)
+plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
+
+# try the function with different values for rr
+rr=.95
+KK=100
+ttMax=50
+NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
+NN[1] <- N0
+for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+print(NN)
+discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun(N0=10, rr=0.95, KK=100, ttMax=50)
+plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
+
+rr=.10
+KK=100
+ttMax=50
+NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
+NN[1] <- N0
+for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+print(NN)
+discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun(N0=10, rr=0.10, KK=100, ttMax=50)
+plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
+
+# the higher rr is the steeper the curve and quicker the population reaches carrying capacity
+
+# with a different KK
+rr=.5
+KK=1000
+ttMax=50
+NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
+NN[1] <- N0
+for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+print(NN)
+discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun(N0=10, rr=0.5, KK=1000, ttMax=50)
+plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
+
+rr=.95
+KK=1000
+ttMax=50
+NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
+NN[1] <- N0
+for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+print(NN)
+discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun(N0=10, rr=0.95, KK=1000, ttMax=50)
+plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
