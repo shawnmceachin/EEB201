7f5ad407a8af31f8029f168403640d123f38528e
shawnmceachin <mceachinshawn@gmail.com>
2015-09-16 17:46:04 -0700

    Add more class notes

--- class_notes_day_1.R
+++ class_notes_day_1.R
@@ -101,3 +101,27 @@ print(xx)
 xx<-1
 while(xx<10){xx <- xx+1;cat("value of xx", xx, "\n"); if(xx==7){cat(" lucky number ", xx, "\n")} else {cat(" not excited about the number ", xx, "\n")}}
 print(xx)
+
+# it's a good idea to use double letters for variable so they're easy to search for
+
+# pseudo code
+# start with base population with number of indivduals N=100 at time t=0
+# loop over t for years 1 to 10 add N*R to N where R=1.05
+# set the initial conditions and parameters; set ttMax to the last year wanted to be calculated
+N0 <- 100
+RR <- 1.05
+ttMax=10
+# create a matrix with 1 row and ttMax+1 columns
+NN <- matrix(NA, nrow=1, ncol=ttMax+1)
+# set the initial value of the matrix to N0
+NN[1]=N0
+# use a loop to run the calculation ttMax times
+for(tt in 1:ttMax){NN[tt+1] <- RR*NN[tt]}
+print(NN)
+plot(1:(ttMax+1), NN, xlab="time", ylab="N", col="blue")
+
+# to make a script into a function
+geomFun <- function(N0, RR, ttMax){NN <- rep(NA, ttMax+1); NN[1]=N0; for(tt in 1:ttMax){NN[tt+1]<-RR*NN[tt]}}
+ttMax=10
+geomFun(N0=10, RR=0.95, ttMax)
+geomFun(RR=1.01, N0=200, ttMax=20)
