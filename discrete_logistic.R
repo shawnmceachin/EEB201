e03784132eeb20df7386ab942f45bbdb74188e1a
shawnmceachin <mceachinshawn@gmail.com>
2015-09-20 18:27:25 -0700

    Create logistic growth function and plots
    and play around with the parameters

--- discrete_logistic.R
+++ discrete_logistic.R
@@ -5,13 +5,25 @@ KK=100
 ttMax=50
 NN <- matrix(NA, nrow=1, ncol=(ttMax+1))
 NN[1] <- N0
-for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}
+for(tt in (1:ttMax)){
+  NN[tt+1] <- NN[tt]*(1+rr*(1-NN[tt]/KK))
+  }
 print(NN)
+plot(1:(ttMax+1), NN, xlab="time", ylab="N", type="l", col="blue")
 
+# Exercise 3.3.1
 # convert the script into a function and plot it
-discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
+discreteLogisticFun <- function(N0, rr, KK, ttMax){
+  NN <- matrix(NA, nrow=1, ncol=ttMax+1)
+  NN[1] <- N0
+  for(tt in 1:ttMax){
+    NN[tt+1] <- NN[tt]*(1+rr*(1-NN[tt]/KK))
+  }
+  plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue", type="l")
+}
+# the plot function MUST go in the function
+  
 discreteLogisticFun(N0=10, rr=0.5, KK=100, ttMax=50)
-plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
 
 # try the function with different values for rr
 rr=.95
@@ -60,3 +72,12 @@ print(NN)
 discreteLogisticFun <- function(N0, rr, KK, ttMax){NN <- matrix(NA, nrow=1, ncol=(ttMax+1)); NN[1] <- N0; for(tt in (1:ttMax)){NN[tt+1] <- NN[tt]*(1+(rr*(1-(NN[tt]/KK))))}}
 discreteLogisticFun(N0=10, rr=0.95, KK=1000, ttMax=50)
 plot(1:(ttMax+1), NN, xlab="time", ylab="pop", col="blue")
+
+# Exercise 3.3.2
+NO = 10
+KK = 100
+rrVec <- c(-0.3, 0.3, 1.3, 1.9, 2.2, 2.7)
+par(mfrow = c(2,3))
+for(ii in 1:length(rrVec)) {
+  discreteLogisticFun(N0=10, rr=ii, KK=100, ttMax=50)
+}
