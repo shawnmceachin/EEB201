d7ba7919d0f7622eaafd7e9074ec296f613d8014
shawnmceachin <mceachinshawn@gmail.com>
2015-09-20 18:28:07 -0700

    refine geometric growth model

--- geometric_growth.R
+++ geometric_growth.R
@@ -15,6 +15,7 @@ for (tt in 1:ttMax) {
 # Plot the results
 plot(1:(ttMax+1),NN, xlab="time", ylab="N", type="b", col="blue")
 
+# Exercise 3.2.1
 # to make the population decline
 N0 <- 25
 RR <- 0.95
@@ -80,6 +81,7 @@ for (tt in 1:ttMax) {
 # Plot the results
 plot(1:(ttMax+1),NN, xlab="time", ylab="N", type="b", col="blue")
 
+# Exercise 3.2.2
 # to make the script into a function
 geometricGrowthFun <- function(N0, RR, ttMax){NN <- matrix(NA, nrow=1, ncol=ttMax+1); NN[1] <- N0; for (tt in 1:ttMax){NN[tt+1] <- RR*NN[tt]}}
 geometricGrowthFun(N0=10, RR=0.95, ttMax=100)
